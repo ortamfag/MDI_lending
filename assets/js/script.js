eecb23703dd435a204d6079c036aea7f673bf05a
ortamfag <a_zhidov@outlook.com>
2022-07-19 18:05:34 +0300

    really fix last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,7 @@ opacity()
 const modalEl = document.querySelector('#modalPopup'),
       btnEl1 = document.querySelector('#modal__preview-1'),
       btnEl2 = document.querySelector('#modal__preview-2'),
+      btnEl3 = document.querySelector('#modal__preview-3'),
       closeEl = document.querySelector('#modalCross'),
       closeBody = document.querySelector('.popup'),
       bg = document.querySelector('#modalBg')
